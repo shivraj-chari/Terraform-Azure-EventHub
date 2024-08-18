cd611aaede23048c999ea88f7e381ddaa8e6d063
Shivraj Chari <shivraj.goa@gmail.com>
2024-08-18 19:49:19 +0530

    updated module

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "azurerm_notification_hub_namespace_id" {
-  value = azurerm_notification_hub_namespace.notification_hub_namespace.id
+  value       = azurerm_notification_hub_namespace.notification_hub_namespace.id
   description = "The ID of the Notification Hub Namespace"
 }
 
 output "azurerm_notification_hub_namespace_servicebus_endpoint" {
-  value = azurerm_notification_hub_namespace.notification_hub_namespace.servicebus_endpoint
+  value       = azurerm_notification_hub_namespace.notification_hub_namespace.servicebus_endpoint
   description = "The ServiceBus Endpoint for this Notification Hub Namespace."
 }
 
 output "azurerm_notification_hub_id" {
-  value = azurerm_notification_hub.notification_hub.id
+  value       = azurerm_notification_hub.notification_hub.id
   description = "The ID of the Notification Hub."
 }
